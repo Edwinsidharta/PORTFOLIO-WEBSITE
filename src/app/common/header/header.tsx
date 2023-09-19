23bb8546c425aa964d46328c0a0f9a1b77e4ec58
Edwinsidharta <edwin0sidharta@gmail.com>
2023-09-19 18:44:09 +0700

    feat : make greeting page

--- src/app/common/header/header.tsx
+++ src/app/common/header/header.tsx
@@ -1,13 +1,34 @@
 "use client";
 
 import Image from 'next/image'
-import useScrollDirection from "@/app/common/use-scroll-direction/useScrollDirection";
+
+import {useEffect, useState} from "react";
+
+export function useScrollDirection() {
+    const [scrollDirection, setScrollDirection] = useState('');
+    useEffect(() => {
+        let lastScrollY = window.pageYOffset;
+        const updateScrollDirection = () => {
+            const scrollY = window.pageYOffset;
+            const direction = scrollY > lastScrollY ? 'down' : 'up';
+            if (direction !== scrollDirection) {
+                setScrollDirection(direction)
+            }
+            lastScrollY = scrollY > 0 ? scrollY : 0;
+        };
+        window.addEventListener("scroll", updateScrollDirection);
+        return () => {
+            window.removeEventListener("scroll", updateScrollDirection); // clean up
+        }
+    }, [scrollDirection])
+    return scrollDirection;
+};
 
 export default function Header() {
     const scrollDirection = useScrollDirection();
     return (
         <main
-            className={`sticky ${scrollDirection === 'down' ? '-top-[80px]' : 'top-0'} bg-white py-[22px] px-[5%] h-[80px] transition-all duration-500`}>
+            className={`sticky ${scrollDirection === 'down' ? '-top-[80px]' : 'top-0'} bg-white py-[22px] px-[5%] h-[80px] transition-all duration-300`}>
             <nav className='flex justify-between'>
                 <div className='flex w-[50%] items-center gap-[8px]'>
                     <Image
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Greeting from "@/app/component/greeting/greeting";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black h-[2000px]">
-      <h1 className={"text-white"}>HELLO WORLD</h1>
+    <main className="flex flex-col items-center bg-white">
+        <Greeting/>
     </main>
   )
 }
